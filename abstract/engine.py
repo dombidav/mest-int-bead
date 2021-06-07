46f4e8860f3c38240e695102fba59764a13e0d5a
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-06-08 00:25:16 +0200

    Added Solver Engine Abstraction

---
+++ abstract/engine.py
@@ -0,0 +1,32 @@
+from abc import ABCMeta, abstractmethod
+import time
+from queue import PriorityQueue
+from typing import Optional
+
+from abstract.node import Node
+from models.state import State
+
+
+class SolverEngine(metaclass=ABCMeta):
+    def __init__(self, start: State, goal: State):
+        self.path: list[State] = []
+        self.visited_queue: list[State] = []
+        self.priority_queue = PriorityQueue()
+        self.start: State = start
+        self.goal: State = goal
+        self.execution_time: float = 0
+
+    def solve(self) -> Optional[list[State]]:
+        start_time = time.time()
+        result = self.implementation()
+        self.execution_time = time.time() - start_time
+        return result
+
+    @abstractmethod
+    def implementation(self) -> Optional[list[State]]:
+        pass
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
