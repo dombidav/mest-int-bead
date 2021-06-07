b4c2f343b8d47e73397e542423e375afb72a6df6
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-06-07 23:46:05 +0200

    Added State Model

---
+++ models/state.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from models.person import Person
+
+
+class State(object):
+    def __init__(self, left_side: list[Person], right_side: list[Person] = None, remaining_minutes: int = 0,
+                 lamp_on_left: bool = True):
+        super(State, self).__init__()
+
+        self.left_side = left_side
+        self.right_side = right_side or []
+        self.remaining_minutes = remaining_minutes
+        self.lamp_on_left = lamp_on_left
+
+    def __eq__(self, other):
+        if not isinstance(other, State):
+            return False
+
+        return self.left_side == other.left_side
+
+    def __str__(self):
+        left_side = '[' + ', '.join(map(str, self.left_side)) + (', *' if self.lamp_on_left else '') + ']'
+        right_side = '[' + ', '.join(map(str, self.right_side)) + ('' if self.lamp_on_left else ', *') + ']'
+        return left_side + ' -- ' + right_side
+
+
+def short_start() -> State:
+    """
+    This is an easy problem for debugging purposes
+    :return: State with only three person and plenty of time
+    """
+    return State([Person(1), Person(2), Person(5)], remaining_minutes=11)
+
+
+def target_state(starting_state: State) -> State:
+    return State([], starting_state.left_side[:], 0, False)
+
+
+def start_state() -> State:
+    return State([Person(1), Person(2), Person(5), Person(10)], remaining_minutes=17)
