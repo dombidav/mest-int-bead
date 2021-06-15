233b0758f7218234d4bb254e04a71dbe7bac600c
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-06-15 11:20:20 +0200

    Reworked main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from application.presentation import print_solution
-from engines.astar import AStarSolver
-from models.state import *
+from application.interaction import clear_output, confirm
+from application.presentation import *
 
 if __name__ == '__main__':
-    print('Starting...')
-    start = start_state()
-    a = AStarSolver(start, target_state(start))
-    solution = a.solve()
-    print_solution(solution, a)
+    while True:
+        show_welcome()
+        solver = ask_for_solver()
+        clear_output()
+        start = ask_for_start_state()
+        target = ask_for_target_state(start)
+        print_solution(solver(start, target))
+        if not confirm('Solver is done. Rerun?'):
+            exit(1)
