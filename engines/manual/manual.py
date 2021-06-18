187cd3344baed6e2c6134f8eba6b018234946554
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-06-18 13:40:33 +0200

    Implemented manual solver

---
+++ engines/manual/manual.py
@@ -0,0 +1,61 @@
+import re
+from typing import Optional
+from more_itertools import last
+
+from abstract.engine import SolverEngine
+from application.interaction import clear_output
+from models.operations import move_persons, is_valid
+from models.person import find_person
+from models.state import State, target_state
+
+
+def ask_move(current_state):
+    clear_output()
+    print("""
+    Move person(s) from one side to another, by sending their numbers. You can send multiple people by simply writing both numbers separated by a comma. No need to specify sides.
+    Z - undo your last move
+    Q - Exit
+    ex: 
+    Send only one person: `3`
+    Send two people: `3, 2`
+    """)
+    while True:
+        print('')
+        print(f'Current state: {current_state} | remaining minutes: {current_state.remaining_minutes}')
+        ans = input('Move:').strip(' ').replace(' ', '')
+        while not re.search("^[0-9zq]+,?[0-9]*$", ans):
+            ans = input('Unknown command. Move:').strip(' ').replace(' ', '')
+        split = ans.split(',')
+        if split[0].lower() == 'z':
+            return None
+        elif split[0].lower() == 'q':
+            exit(0)
+        if len(split) == 1:
+            moving_persons = [find_person(split[0], current_state)]
+        else:
+            moving_persons = [find_person(split[0], current_state), find_person(split[1], current_state)]
+        if not is_valid(current_state, moving_persons):
+            print('Invalid move.')
+            continue
+        return move_persons(current_state, moving_persons)
+
+
+class ManualSolver(SolverEngine):
+
+    @property
+    def name(self):
+        return 'Manually solved'
+
+    def __init__(self, start, goal):
+        super(ManualSolver, self).__init__(start, goal)
+        self.stack = [start]
+
+    def implementation(self) -> Optional[list[State]]:
+        while target_state(self.start) != last(self.stack):
+            current_state = last(self.stack)
+            new_state = ask_move(current_state)
+            if new_state is None:
+                self.stack.pop()
+            else:
+                self.stack.append(new_state)
+        return self.stack
