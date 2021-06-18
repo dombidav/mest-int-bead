010bbe41262818aa7e4f7e785aeb998d5d1ec8ea
Dombi Tibor <40638464+dombidav@users.noreply.github.com>
2021-06-18 14:01:31 +0200

    Implemented Greedy best-first search

--- engines/greedy/greedy.py
+++ engines/greedy/greedy.py
@@ -1,16 +1,33 @@
 from typing import Optional
 
 from abstract.engine import SolverEngine
+from engines.greedy.greedy_node import GreedyNode
 from models.state import State
 
 
 class GreedySolver(SolverEngine):
     @property
     def name(self):
-        return 'Greedy Best First algorithm (Greedy search)'
+        return 'Greedy BFS algorithm (Greedy search)'
 
     def __init__(self, start, goal):
         super(GreedySolver, self).__init__(start, goal)
 
     def implementation(self) -> Optional[list[State]]:
-        pass
+        start_node = GreedyNode(self.start, None, 0)
+        child_id = 0
+        self.priority_queue.put((0, child_id, start_node))
+        while not self.path and self.priority_queue.qsize():
+            closest_child: GreedyNode = self.priority_queue.get()[2]
+            closest_child.create_children()
+            self.visited_queue.append(closest_child.value)
+            for child in closest_child.children:
+                if child.value not in self.visited_queue:
+                    child_id += 1
+                    if child.value == self.goal:
+                        self.path = child.path
+                        break
+                    self.priority_queue.put((child.cost, child_id, child))
+        if not self.path:
+            return None
+        return self.path
